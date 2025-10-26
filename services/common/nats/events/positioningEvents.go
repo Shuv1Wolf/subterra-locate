79f2fdabe4e6ba6b6cb39b078b78b621a58a9526
Shuv1Wolf <mskuratik06@gmail.com>
2025-10-26 21:38:31 +0300

    updated nats common lib

--- services/common/nats/events/positioningEvents.go
+++ services/common/nats/events/positioningEvents.go
@@ -3,9 +3,11 @@ package events
 import "time"
 
 type DevicePositioningEventV1 struct {
-	DeviceId string    `json:"device_id"`
-	X        float64   `json:"x"`
-	Y        float64   `json:"y"`
-	Z        float64   `json:"z"`
-	Time     time.Time `json:"time"`
+	DeviceId  string    `json:"device_id"`
+	MapId     string    `json:"map_id"`
+	OrgId     string    `json:"org_id"`
+	X         float64   `json:"x"`
+	Y         float64   `json:"y"`
+	Z         float64   `json:"z"`
+	Timestamp time.Time `json:"timestamp"`
 }
