522baf05c5b87f18ab3ced53e0ac7b0343b17e90
Shuv1Wolf <mskuratik06@gmail.com>
2025-11-08 18:38:22 +0300

    added RequestContextV1

--- services/common/data/version1/RequestContextV1.go
+++ services/common/data/version1/RequestContextV1.go
@@ -1,6 +1,32 @@
 package data1
 
+import (
+	cconv "github.com/pip-services4/pip-services4-go/pip-services4-commons-go/convert"
+	"github.com/pip-services4/pip-services4-go/pip-services4-commons-go/data"
+	cvalid "github.com/pip-services4/pip-services4-go/pip-services4-data-go/validate"
+)
+
 type RequestContextV1 struct {
 	OrgId  string `json:"org_id"`
 	UserId string `json:"user_id"`
+}
+
+func NewRequestContextV1Schema() *cvalid.ObjectSchema {
+	return cvalid.NewObjectSchema().
+		WithOptionalProperty("org_id", cconv.String).
+		WithOptionalProperty("user_id", cconv.Boolean)
+}
+
+func NewRequestContextV1FromValue(value any) *RequestContextV1 {
+	if v, ok := value.(*RequestContextV1); ok {
+		return v
+	}
+	return NewRequestContextV1FromMap(data.NewAnyValueMapFromValue(value))
+}
+
+func NewRequestContextV1FromMap(value *data.AnyValueMap) *RequestContextV1 {
+	return &RequestContextV1{
+		OrgId:  value.GetAsString("org_id"),
+		UserId: value.GetAsString("user_id"),
+	}
 }
