62680d532edcccb2386d75f0219099b0acdfa3e1
Shuv1Wolf <mskuratik06@gmail.com>
2025-11-22 19:29:53 +0300

    small fix

---
+++ clients/zone-processor/build/ZoneProcessorClientsFactory.go
@@ -0,0 +1,23 @@
+package build
+
+import (
+	clients1 "github.com/Shuv1Wolf/subterra-locate/clients/zone-processor/clients/version1"
+	cbuild "github.com/pip-services4/pip-services4-go/pip-services4-components-go/build"
+	cref "github.com/pip-services4/pip-services4-go/pip-services4-components-go/refer"
+)
+
+type ZoneProcessorClientsFactory struct {
+	cbuild.Factory
+	GrpcClientDescriptor *cref.Descriptor
+}
+
+func NewZoneProcessorClientsFactory() *ZoneProcessorClientsFactory {
+
+	bcf := ZoneProcessorClientsFactory{}
+	bcf.Factory = *cbuild.NewFactory()
+
+	bcf.GrpcClientDescriptor = cref.NewDescriptor("zone-processor", "client", "grpc", "*", "1.0")
+	bcf.RegisterType(bcf.GrpcClientDescriptor, clients1.NewZoneGrpcClientV1)
+
+	return &bcf
+}
